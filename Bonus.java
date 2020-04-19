316ea40a39f36eff281d748d9f57ed4e73e7d756
VeraOm <aug_ooy@tutanota.com>
2020-04-19 17:20:53 +0600

    Исправление замечаний

--- Bonus.java
+++ Bonus.java
@@ -1,20 +1,20 @@
 public class Bonus {
 
-        public static void main(String[] args) {
-            int AmountSum=10;
-            int PaymentSum=1100;
-            System.out.printf("Количество денег на текущем счёте - %d", AmountSum);
-            System.out.println();
-            System.out.printf("Сумма пополнения - %d", PaymentSum);
-            System.out.println();
+    public static void main(String[] args) {
+        int amountSum = 10;
+        int paymentSum = 1100;
+        System.out.printf("Количество денег на текущем счёте - %d", amountSum);
+        System.out.println();
+        System.out.printf("Сумма пополнения - %d", paymentSum);
+        System.out.println();
 
-            if(PaymentSum > 1000){
-                AmountSum=AmountSum+PaymentSum+(PaymentSum/100);
-            }else{
-                AmountSum=AmountSum+PaymentSum;
-            }
-
-            System.out.printf("Итоговая сумма - %d", AmountSum);
+        if (paymentSum > 1000) {
+            amountSum = amountSum + paymentSum + (paymentSum / 100);
+        } else {
+            amountSum = amountSum + paymentSum;
         }
+
+        System.out.printf("Итоговая сумма - %d", amountSum);
+    }
 
 }
